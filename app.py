20a12efc04699b58191feff945b62a6ea4f5feb7
WilliBobadilla <Willi1997.1@gmail.com>
2020-02-20 14:12:09 -0300

    subida de archivos

---
+++ app.py
@@ -0,0 +1,35 @@
+#app.py
+
+from flask import Flask, request #import main Flask class and request object
+
+
+import csv 
+app = Flask(__name__) #create the Flask app
+
+@app.route('/')
+def query_example():
+    return 'Probandooooooo'
+
+@app.route('/form-example',methods = ['POST'])
+def formexample():
+    if request.method == 'POST':
+        nombre = request.form["nombre"]
+        apellido=request.form["apellido"]
+    with open("datos_nuevos.csv","a+") as data_temp_csv:
+        # agregar lo de leer si es que ya esta escrito open
+        # la cabecera, la posible solucion es de abrir el 
+        #archivo y verificar si es que ya esta o no 
+        nombrefilas= ["Nombre","Apellido"]  # nombre de los campos
+        writer=csv.DictWriter(data_temp_csv,fieldnames=nombrefilas) # creamos un objeto writer sobre el archivo, y le especificamos el nombre de kis campos
+        #writer.writeheader() #escribimos la primera
+
+        writer.writerow({'Nombre' : nombre, 'Apellido':apellido})
+
+    return '<h2> Recibido!  </h2>'
+
+@app.route('/json')
+def jsonexample():
+    return 'Todo...'
+
+if __name__== '__main_':
+    app.run(debug=True, port=5000) #run app in debug mode on port 5000
